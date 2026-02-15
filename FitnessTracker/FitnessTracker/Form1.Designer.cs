cc68f54004cac5059143d64212e3746f353959e5
szotyifej <gruada845@logiker.hu>
2026-02-15 07:46:28 +0100

    Import and Export labels added

--- FitnessTracker/FitnessTracker/Form1.Designer.cs
+++ FitnessTracker/FitnessTracker/Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.txtHelyszin = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.btnFelvitel = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudIdotartam)).BeginInit();
             this.SuspendLayout();
             // 
@@ -115,11 +121,74 @@
             this.btnFelvitel.UseVisualStyleBackColor = true;
             this.btnFelvitel.Click += new System.EventHandler(this.btnFelvitel_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Manual upload";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Import File";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(334, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Export File";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(42, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(387, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Notice: when importing a file, the filename must match \"sport.csv\"";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(170, 198);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(135, 39);
+            this.btnImport.TabIndex = 15;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(45, 206);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(100, 22);
+            this.txtPath.TabIndex = 16;
+            this.txtPath.Text = "FilePath";
+            this.txtPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnFelvitel);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtHelyszin);
@@ -149,6 +218,12 @@
         private System.Windows.Forms.TextBox txtHelyszin;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnFelvitel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox txtPath;
     }
 }
 
